3a4ce08ddcb1f1c544c8e7342e59e22679deacae
RGSP <reznote1401@gmail.com>
2026-08-06 19:09:43 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ int main(int argc, const char* argv[]){
                     Ofor.writeIn(out, ln);
                 }
             }
-                cout<<"file copied succesfully" <<endl;
+            cout<<"file copied succesfully" <<endl;
     }
         return 0;
 }
