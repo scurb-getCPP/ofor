e912d124591e70c06d159870b898827b031e5a77
RGSP <reznote1401@gmail.com>
2026-08-08 10:15:31 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,59 +24,58 @@ int main(int argc, const char* argv[]){
 
         iss>> openCommand>> copyCommand >>editCommand;
 
-        for(auto fi = argv + 2; fi != argv + argc; ++fi){
+        if(argv[1] == openCommand){
+                for(auto fi = argv + 2; fi != argv + argc; ++fi){
+                ifstream file(*fi);
 
-                if(argv[1] == openCommand){
-                        ifstream file(*fi);
-
-                        if(!file) {
-                                cerr<<"file cant open : " <<*fi <<endl;
-                        }
-                        else{
-                                Ofor.openFile(file);
-                                file.close();
-                        }
+                if(!file) {
+                        cerr<<"file cant open : " <<*fi <<endl;
                 }
-                else if(argv[1] == copyCommand){
-                        string text;
-                        vector<string> texts;
-
-                        ifstream file1(argv[2]);
-                        ofstream file2(argv[3], ios::app);
-
-                        if(!file1){
-                                cerr<<"cannot open : " <<argv[2] <<endl;
-                        }
-                        if(!file2){
-                                cerr<<"cannot open/create : " <<argv[3] <<endl;
-                        }
-
-                        while(getline(file1, text)){
-                                texts.push_back(text);
-                        }
-
-                        for(const string& line : texts){
-                                Ofor.writeIn(file2, line);
-                        }
-                        file1.close();
-                        file2.close();
+                else{
+                        Ofor.openFile(file);
+                        file.close();
                 }
-                else if(argv[1] == editCommand){
-                        string text;
-                        ofstream file(argv[2], ios::app);
-
-                        if(!file){
-                                cerr<<"file not exist : " <<argv[2] <<endl;
-                        }
-                        else{
-                                cout<<"enter your text/code : " <<endl;
-                                while(getline(cin, text)){
-                                        if(text == "exit")
-                                                break;
-                                        Ofor.writeIn(file, text);
-                                }
-                        }file.close();
                 }
+        }
+        else if(argv[1] == copyCommand){
+                string text;
+                vector<string> texts;
+
+                ifstream file1(argv[2]);
+                ofstream file2(argv[3], ios::app);
+
+                if(!file1){
+                        cerr<<"cannot open : " <<argv[2] <<endl;
+                }
+                if(!file2){
+                        cerr<<"cannot open/create : " <<argv[3] <<endl;
+                }
+
+                while(getline(file1, text)){
+                        texts.push_back(text);
+                }
+
+                for(const string& line : texts){
+                        Ofor.writeIn(file2, line);
+                }
+                file1.close();
+                file2.close();
+                }
+        else if(argv[1] == editCommand){
+                string text;
+                ofstream file(argv[2], ios::app);
+
+                if(!file){
+                        cerr<<"file not exist : " <<argv[2] <<endl;
+                }
+                else{
+                        cout<<"enter your text/code : " <<endl;
+                        while(getline(cin, text)){
+                                if(text == "exit")
+                                        break;
+                                Ofor.writeIn(file, text);
+                        }
+                }        file.close();
         }
 
         return 0;
